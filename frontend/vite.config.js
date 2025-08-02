621f0c600b287a62ea66271bd1a96eed0bd20fb1
brjatoba92 <brjatoba92@gmail.com>
2025-08-02 15:46:48 -0300

    feat: Inicio do frontend com vite e tailwind

---
+++ frontend/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          utils: ['axios', 'react-hook-form']
+        }
+      }
+    }
+  }
+})
